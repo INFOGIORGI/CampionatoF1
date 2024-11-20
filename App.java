6eb6cc9930e9d815439c7c335cfa350fd8b58f8b
Luciano Schiavone <luciano.schiavone@ittgiorgi.edu.it>
2024-11-20 11:04:58 +0100

    Create App.java

---
+++ App.java
@@ -0,0 +1,54 @@
+public class App {
+    public static void main(String[] args) {
+      /*
+        Pilota leclerc = new Pilota("Charles","Leclerc","Monaco",16);
+        Pilota sainz = new Pilota("Carlos","Sainz","Spagna",55);
+        Pilota piastri = new Pilota("Oscar","Piastri","Australia",44);
+        Pilota norris = new Pilota("Lando","Norris","Gran Bretagna",21);
+        Pilota verstappen = new Pilota("Max","Verstappen","Olanda",10);
+        Pilota perez = new Pilota("Sergio","Perez","Messico",13);
+      
+        Squadra ferrari = new Squadra("Ferrari", "Italia");
+        ferrari.addPilota(leclerc);
+        ferrari.addPilota(sainz);
+
+        System.out.println(ferrari); //Stampa squadra
+    */
+    /*
+        Squadra redBull = new Squadra("Red Bull","Austria");
+        redBull.addPilota(verstappen);
+        redBull.addPilota(perez);
+
+        Squadra mclaren = new Squadra("McLaren","Gran Bretagna");
+        mclaren.addPilota(piastri);
+        mclaren.addPilota(norris);
+    */
+    /*
+        GranPremio monza = new GranPremio("Monza", "Italia", 3.248, 71);
+        GranPremio imola = new GranPremio("Imola", "Italia", 2.248, 78);
+        monza.setOrdine(leclerc,piastri,sainz);
+        imola.setOrdine(verstappen,sainz,norris);
+
+        System.out.println(monza); //Stampa dati circuito
+
+        System.out.println(monza.getOrdine()); //Stampa podio
+    */
+    /*
+        CampionatoF1 champ = new CampionatoF1();
+        champ.addSquadra(ferrari);
+        champ.addSquadra(mclaren);
+        champ.addSquadra(redBull);
+
+        champ.addGP(monza);
+        champ.addGP(imola);
+    */
+    //  System.out.println(champ.getGP());
+
+    //  System.out.println(champ.getSquadre());
+
+    //  System.out.println(champ.classificaCostruttori());
+
+    //  System.out.println(champ.classificaPiloti());
+
+    }
+}
